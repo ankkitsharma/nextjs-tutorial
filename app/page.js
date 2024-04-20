5c91261608eb7338b9738c5e921e00ce33ad4487
Ankit Sharma <ankitsharma2801@gmail.com>
2024-04-20 11:54:04 +0530

    add counter

--- app/page.js
+++ app/page.js
@@ -4,9 +4,9 @@ import Link from "next/link";
 const page = () => {
   return (
     <div>
-      <h1 className="text-7xl">Home Page</h1>
-      <Link href="/about" className="text-2xl">
-        About
+      <h1 className="text-7xl mb-8 font-bold">Next.js Tutorial</h1>
+      <Link href="/client" className="btn btn-accent">
+        Get Started
       </Link>
     </div>
   );
