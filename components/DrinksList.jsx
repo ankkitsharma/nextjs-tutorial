a7e7c60b2e85f7b98cd2e84718eb4d2aff274c99
Ankit Sharma <ankitsharma2801@gmail.com>
2024-04-21 16:26:33 +0530

    add remove images to drinkslist

--- components/DrinksList.jsx
+++ components/DrinksList.jsx
@@ -1,15 +1,25 @@
 import React from "react";
 import Link from "next/link";
+import Image from "next/image";
 
 function DrinksList({ drinks }) {
   return (
-    <ul className="menu menu-vertical pl-0">
+    <ul className="grid sm:grid-cols-2 gap-6 mt-6">
       {drinks.map((drink) => (
         <li key={drink.idDrink}>
           <Link
             href={`/drinks/${drink.idDrink}`}
             className="text-xl font-medium"
           >
+            <div className="relative h-48 mb-4">
+              <Image
+                src={drink.strDrinkThumb}
+                fill
+                sizes="(max-width:768px) 100vw, (max-width:1200px) 50vw"
+                className="rounded-md object-cover"
+                alt={drink.strDrink}
+              />
+            </div>
             {drink.strDrink}
           </Link>
         </li>
