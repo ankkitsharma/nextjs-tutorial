2845021fa8b95bd05faec641d0b7eb00f68f24ff
Ankit Sharma <ankitsharma2801@gmail.com>
2024-05-16 16:40:05 +0530

    add functionality to deleteform

--- components/DeleteForm.jsx
+++ components/DeleteForm.jsx
@@ -1,10 +1,41 @@
+"use client";
 import { deleteTask } from "@/utils/actions";
+import { useEffect } from "react";
+import { useFormStatus, useFormState } from "react-dom";
+import toast from "react-hot-toast";
+
+const SubmitBtn = () => {
+  const { pending } = useFormStatus();
+  return (
+    <button type="submit" disabled={pending} className="btn btn-error btn-xs">
+      {pending ? "deleting..." : "delete"}
+    </button>
+  );
+};
+
+const initialState = {
+  message: null,
+};
 
 const DeleteForm = ({ id }) => {
+  const [state, formAction] = useFormState(deleteTask, initialState);
+
+  useEffect(() => {
+    if (state.message === "error") {
+      toast.error("there was an error");
+      return;
+    }
+
+    if (state.message === "success") {
+      toast.success("task deleted");
+      return;
+    }
+  }, [state.message]);
+
   return (
-    <form action={deleteTask}>
+    <form action={formAction}>
       <input type="hidden" name="id" value={id} />
-      <button className="btn btn-error btn-xs">delete</button>
+      <SubmitBtn />
     </form>
   );
 };
--- utils/actions.js
+++ utils/actions.js
@@ -3,6 +3,7 @@
 import prisma from "@/utils/db";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
+import toast from "react-hot-toast";
 import { z } from "zod";
 
 export const getAllTasks = async () => {
@@ -26,10 +27,18 @@ export const createTask = async (formData) => {
   revalidatePath("/tasks");
 };
 
-export const deleteTask = async (formData) => {
+export const deleteTask = async (prevState, formData) => {
+  await new Promise((resolve) => {
+    setTimeout(resolve, 1000);
+  });
   const id = formData.get("id");
-  await prisma.task.delete({ where: { id } });
-  revalidatePath("/tasks");
+  try {
+    await prisma.task.delete({ where: { id } });
+    revalidatePath("/tasks");
+    return { message: "success" };
+  } catch (error) {
+    return { message: "error" };
+  }
 };
 
 export const getTask = async (id) => {
@@ -60,7 +69,7 @@ export const editTask = async (formData) => {
 };
 
 export const createTaskCustom = async (prevState, formData) => {
-  await new Promise((resolve) => setTimeout(resolve, 2000));
+  await new Promise((resolve) => setTimeout(resolve, 1000));
   const content = formData.get("content");
   const task = z.object({
     content: z.string().min(5),
